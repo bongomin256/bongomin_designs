657e2d8bae24a26625919ee9050a01f66ddc7717
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-04-11 16:20:49 -0700

    added all the sections in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,61 @@
+import { TbWorldWww } from "react-icons/tb";
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <section>
+        <section>
+          <div>
+            <div>
+              <p>Bongomin Designs</p>
+            </div>
+            <div>
+              <p>
+                <span>Phone:</span> 4253950516
+              </p>
+              <p>
+                <span>Email:</span> info@domain.com
+              </p>
+            </div>
+            <div>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <TbWorldWww />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+              </a>
+
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact Us</li>
+          </ul>
+        </section>
+        <section>
+          <div>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <p>Terms & Conditions</p>
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <p>Privacy Policy</p>
+            </a>
+          </div>
+          <p>&copy 2024 Company Name. All rights reserved</p>
+        </section>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
