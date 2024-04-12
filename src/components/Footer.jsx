ee49c686eb5692ccf3613c62804e74e5cc939cf8
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-04-11 17:29:30 -0700

    added styles to the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,38 +3,38 @@ import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="bg-[#15252d] py-[3.75rem] px-[1.5rem] md:px-[2rem] lg:px-[8.063rem]">
       <section>
-        <section>
+        <section className="flex items-center gap-12 flex-col w-full md:flex-row md:justify-between md:items-start ">
           <div>
             <div>
               <p>Bongomin Designs</p>
             </div>
-            <div>
-              <p>
-                <span>Phone:</span> 4253950516
+            <div className="mt-8 text-center font-navFont font-semibold text-base1 ">
+              <p className="mb-2">
+                <span className="text-cta-pri">Phone:</span> 4253950516
               </p>
               <p>
-                <span>Email:</span> info@domain.com
+                <span className="text-cta-pri">Email:</span> info@domain.com
               </p>
             </div>
-            <div>
+            <div className="flex justify-between text-cta-pri mt-5">
               <a href="http://" target="_blank" rel="noopener noreferrer">
-                <TbWorldWww />
+                <TbWorldWww size={32} />
               </a>
               <a href="http://" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin />
+                <FaLinkedin size={32} />
               </a>
               <a href="http://" target="_blank" rel="noopener noreferrer">
-                <FaInstagram />
+                <FaInstagram size={32} />
               </a>
 
               <a href="http://" target="_blank" rel="noopener noreferrer">
-                <FaGithub />
+                <FaGithub size={32} />
               </a>
             </div>
           </div>
-          <ul>
+          <ul className="flex flex-col text-center gap-6 text-base1 md:text-end lg:flex-row">
             <li>Home</li>
             <li>About Us</li>
             <li>Services</li>
@@ -42,8 +42,8 @@ const Footer = () => {
             <li>Contact Us</li>
           </ul>
         </section>
-        <section>
-          <div>
+        <section className="flex flex-col text-center gap-8 text-base1 font-thin mt-12 lg:flex-row-reverse justify-between lg:text-left">
+          <div className="flex justify-between">
             <a href="http://" target="_blank" rel="noopener noreferrer">
               <p>Terms & Conditions</p>
             </a>
@@ -51,7 +51,7 @@ const Footer = () => {
               <p>Privacy Policy</p>
             </a>
           </div>
-          <p>&copy 2024 Company Name. All rights reserved</p>
+          <p>2024 Company Name. All rights reserved</p>
         </section>
       </section>
     </footer>
