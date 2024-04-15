ce6d7af4cc7613a88d2cee94272c1ad5de99bbe7
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-04-15 16:12:37 -0700

    created a layout and pages folder and also installed react touter dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePages from "./pages/HomePages";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePages />} />
+    </Route>
+  )
+);
+
+// import MainLayout from "./layouts/MainLayout";
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <RouterProvider router={router} />
     </>
   );
 };
---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,5 @@
+const HomePages = () => {
+  return <div className="bg-white text-cta-pri">HomePages</div>;
+};
+
+export default HomePages;
