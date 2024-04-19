b9e68acb04b108fcd49c06f93765bab3c35c7106
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-04-19 10:06:44 -0700

    added styles to our service section

--- src/components/OurServiceHome.jsx
+++ src/components/OurServiceHome.jsx
@@ -1,12 +1,25 @@
 import SectionLayout from "../layouts/SectionLayout";
-
+import data from "../../data.json";
+// import Button from "./Buttons";
+import ServiceCard from "./ServiceCard";
 const OurServiceHome = () => {
+  const services = data.ourServices;
   return (
     <SectionLayout>
       <section>
-        <h3> Our Services</h3>
-        <h2>What we bring to the table</h2>
-        <section></section>
+        <div className="mb-[4.688rem]">
+          <h3 className="mb-6 text-center text-cta-sec text-heading3-sm font-headerFonts">
+            Our Services
+          </h3>
+          <h2 className="text-center text-heading-text text-heading2-sm font-headerFonts">
+            What we bring to the table
+          </h2>
+        </div>
+        <section className="flex flex-col justify-center gap-5 md:flex-row md:flex-wrap lg:flex-nowrap">
+          {/* className="flex flex-col gap-5 md:flex-row md:flex-wrap" */}
+          {/* className="grid gap-5 md:grid-rows-2" */}
+          <ServiceCard services={services} />
+        </section>
       </section>
     </SectionLayout>
   );
--- src/layouts/SectionLayout.jsx
+++ src/layouts/SectionLayout.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 
-const SectionLayout = ({ children }) => {
+const SectionLayout = ({ children, bgDefault = "bg-white" }) => {
   return (
-    <section className="px-[1.5rem] py-16 md:px-[2rem] lg:py-40 lg:px-[8.063rem]">
+    <section
+      className={`${bgDefault} px-[1.5rem] py-16 md:px-[2rem] lg:py-40 lg:px-[8.063rem]`}
+    >
       {children}
     </section>
   );
