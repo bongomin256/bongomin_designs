56775236bbe195f81f038ae47a0e004a4e500216
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-04-19 14:03:57 -0700

    deleted a misspelling in the p tag in the home page

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -6,14 +6,14 @@ const HomePages = () => {
   return (
     <>
       <section className="px-[1.5rem] py-16 md:px-[2rem] lg:py-40 lg:px-[8.063rem]">
-        <section className="flex flex-col lg:flex-row  lg:gap-10 lg:items-center">
-          <section className="text-center z-10 lg:text-left lg:basis-full ">
+        <section className="flex flex-col lg:flex-row lg:gap-10 lg:items-center">
+          <section className="z-10 text-center lg:text-left lg:basis-full ">
             <p className="text-heading1-sm font-headerFonts mb-7 md:text-heading1-lg">
               We help you turn your <span className="text-cta-pri">ideas</span>{" "}
               into a <span className="text-cta-pri">website</span>
             </p>
             <p className="text-base2 font-bodyFont mb-7 md:text-base1 md:leading-7">
-              From talented web developers to creative designers and savvy
+              From talented web developers to creative designers and savy
               digital marketers, our team is equipped with the knowledge and
               experience to meet your unique requirements.
             </p>
