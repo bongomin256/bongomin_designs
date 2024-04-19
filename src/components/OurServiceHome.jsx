817bc0c6431dea007cde1114f75636d38d8dee19
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-04-19 13:43:17 -0700

    added a line before and after the h3 in the header of our service section

--- src/components/OurServiceHome.jsx
+++ src/components/OurServiceHome.jsx
@@ -11,13 +11,25 @@ const OurServiceHome = ({ bgDefault }) => {
     <SectionLayout bgDefault={"bg-white"}>
       <section>
         <div className="mb-[4.688rem]">
+          <div className="flex items-center justify-center gap-3 mb-6 text-center">
+            <span className=" w-[4.5rem] border-b border-cta-sec"></span>
+            <h3 className="inline-block text-cta-sec text-heading3-sm font-headerFonts">
+              Our Services
+            </h3>
+            <span className=" w-[4.5rem] border-b border-cta-sec"></span>
+          </div>
+          <h2 className="text-center text-heading-text text-heading2-sm font-headerFonts">
+            What we bring to the table
+          </h2>
+        </div>
+        {/* <div className="mb-[4.688rem]">
           <h3 className="mb-6 text-center text-cta-sec text-heading3-sm font-headerFonts">
             Our Services
           </h3>
           <h2 className="text-center text-heading-text text-heading2-sm font-headerFonts">
             What we bring to the table
           </h2>
-        </div>
+        </div> */}
         <section className="flex flex-col justify-center gap-5 md:flex-row md:flex-wrap lg:flex-nowrap">
           <ServiceCard services={services} />
         </section>
