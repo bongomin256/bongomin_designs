5ed64498989979c02e3a91193ff4db40ee5d3566
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2024-05-01 11:46:59 -0700

    edited map function in PortfolioCard component

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -5,25 +5,26 @@
 
 const PortfolioCard = ({ category, projects }) => (
   <>
-    {projects.map((project, index) => (
-      <section
-        key={index}
-        className="p-3 rounded-md bg-body-text md:w-[15.208rem] md:h-[15.688rem] lg:w-[23.833rem] lg:h-[15.688rem]"
-      >
-        <div>
+    {projects &&
+      projects.map((project, index) => (
+        <section
+          key={index}
+          className="p-3 rounded-md bg-body-text md:w-[15.208rem] md:h-[15.688rem] lg:w-[23.833rem] lg:h-[15.688rem]"
+        >
           <div>
-            <img
-              src={project.imgUrl}
-              alt={project.projectName}
-              className="w-full rounded-md lg:w-[22.5rem] lg:h-[11.5rem]"
-            />
+            <div>
+              <img
+                src={project.imgUrl}
+                alt={project.projectName}
+                className="w-full rounded-md lg:w-[22.5rem] lg:h-[11.5rem]"
+              />
+            </div>
+            <p className="py-2 text-xl font-semibold leading-9 text-center text-pri-bg lg:text-2xl lg:font-medium">
+              {category}
+            </p>
           </div>
-          <p className="py-2 text-xl font-semibold leading-9 text-center text-pri-bg lg:text-2xl lg:font-medium">
-            {category}
-          </p>
-        </div>
-      </section>
-    ))}
+        </section>
+      ))}
   </>
 );
 
